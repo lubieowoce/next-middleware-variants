0a8aa2ca0d01928952bf69d567cbfdd0d31eebbe
lubieowoce <lolzatu2@gmail.com>
2024-03-05 10:25:38 +0000

    Remove old manually-written config from variants.config.ts

--- app/variants.config.ts
+++ app/variants.config.ts
@@ -1,13 +1,1 @@
-// import { type VariantMatcherConfig } from "@/app/lib/variants/matcher";
-// import { colorVariant, fontVariant } from "./variants";
-
-// export default {
-//   patterns: [
-//     [{ pathname: "/" }, [colorVariant]],
-//     [{ pathname: "/foo" }, [colorVariant, fontVariant]],
-//     [{ pathname: "/bar/:barId" }, [fontVariant, colorVariant]],
-//     [{ pathname: "/dynamic" }, [fontVariant]],
-//   ],
-// } satisfies VariantMatcherConfig;
-
 export { default as default } from "./.variants/config.generated";
